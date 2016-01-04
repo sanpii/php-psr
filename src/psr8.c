e60b92e6395cda27799f6d9de7cf21004e166e9b
Sanpi <sanpi@homecomputing.fr>
2016-01-04 18:18:47 +0100

    Add psr8 module

---
+++ src/psr8.c
@@ -0,0 +1,12 @@
+#ifdef HAVE_CONFIG_H
+#    include "config.h"
+#endif
+
+#include "psr8.h"
+
+/* {{{ PHP_MINIT_FUNCTION */
+PHP_MINIT_FUNCTION(psr8)
+{
+    return SUCCESS;
+}
+/* }}} */
---
+++ src/psr8.h
@@ -0,0 +1,8 @@
+#ifndef PHP_PSR_HUGG_H
+#define PHP_PSR_HUGG_H
+
+#include "php.h"
+
+PHP_MINIT_FUNCTION(psr8);
+
+#endif /* PHP_PSR_HUGG_H */
