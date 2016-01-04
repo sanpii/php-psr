207011775662716ffdade53bac865097913d5e22
Sanpi <sanpi@homecomputing.fr>
2016-01-04 19:15:38 +0100

    Add Psr\Link\LinkInterface interface

--- src/psr13.c
+++ src/psr13.c
@@ -4,9 +4,24 @@
 
 #include "psr13.h"
 
+/* {{{ interface Psr\Link\LinkInterface */
+static zend_function_entry linkInterface_class_fuctions[] = {
+    PHP_ABSTRACT_ME(LinkInterface, getHref, NULL)
+    PHP_ABSTRACT_ME(LinkInterface, isTemplated, NULL)
+    PHP_ABSTRACT_ME(LinkInterface, getRels, NULL)
+    PHP_ABSTRACT_ME(LinkInterface, getAttributes, NULL)
+    PHP_FE_END
+};
+/* }}} */
+
 /* {{{ PHP_MINIT_FUNCTION */
 PHP_MINIT_FUNCTION(psr13)
 {
+    zend_class_entry ce;
+
+    INIT_NS_CLASS_ENTRY(ce, "Psr\\Link", "LinkInterface", linkInterface_class_fuctions);
+    zend_register_internal_interface(&ce TSRMLS_CC);
+
     return SUCCESS;
 }
 /* }}} */
