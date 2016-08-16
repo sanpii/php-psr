464885ec30507c84e082568ceb95de620112b0c2
Sanpi <sanpi@homecomputing.fr>
2016-08-16 14:07:51 +0200

    Add Psr\Link\EvolvableLinkInterface interface

--- src/psr13.c
+++ src/psr13.c
@@ -14,6 +14,38 @@ static zend_function_entry linkInterface_class_fuctions[] = {
 };
 /* }}} */
 
+/* {{{ interface Psr\Link\EvolvableLinkInterface */
+ZEND_BEGIN_ARG_INFO(arginfo_evolvableLinkInterface_withHref, 0)
+    ZEND_ARG_INFO(0, href)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(arginfo_evolvableLinkInterface_withRel, 0)
+    ZEND_ARG_INFO(0, rel)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(arginfo_evolvableLinkInterface_withoutRel, 0)
+    ZEND_ARG_INFO(0, rel)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(arginfo_evolvableLinkInterface_withAttribute, 0)
+    ZEND_ARG_INFO(0, attribute)
+    ZEND_ARG_INFO(0, value)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(arginfo_evolvableLinkInterface_withoutAttribute, 0)
+    ZEND_ARG_INFO(0, attribute)
+ZEND_END_ARG_INFO()
+
+static zend_function_entry evolvableLinkInterface_class_fuctions[] = {
+    PHP_ABSTRACT_ME(EvolvableLinkInterface, withHref, arginfo_evolvableLinkInterface_withHref)
+    PHP_ABSTRACT_ME(EvolvableLinkInterface, withRel, arginfo_evolvableLinkInterface_withRel)
+    PHP_ABSTRACT_ME(EvolvableLinkInterface, withoutRel, arginfo_evolvableLinkInterface_withoutRel)
+    PHP_ABSTRACT_ME(EvolvableLinkInterface, withAttribute, arginfo_evolvableLinkInterface_withAttribute)
+    PHP_ABSTRACT_ME(EvolvableLinkInterface, withoutAttribute, arginfo_evolvableLinkInterface_withoutAttribute)
+    PHP_FE_END
+};
+/* }}} */
+
 /* {{{ interface Psr\Link\LinkCollectionInterface */
 ZEND_BEGIN_ARG_INFO(arginfo_linkCollectionInterface_getLinksByRel, 0)
     ZEND_ARG_INFO(0, rel)
@@ -30,9 +62,15 @@ static zend_function_entry linkCollectionInterface_class_fuctions[] = {
 PHP_MINIT_FUNCTION(psr13)
 {
     zend_class_entry ce;
+    zend_class_entry *linkInterface = NULL;
+    zend_class_entry *evolvableLinkInterface = NULL;
 
     INIT_NS_CLASS_ENTRY(ce, "Psr\\Link", "LinkInterface", linkInterface_class_fuctions);
-    zend_register_internal_interface(&ce TSRMLS_CC);
+    linkInterface = zend_register_internal_interface(&ce TSRMLS_CC);
+
+    INIT_NS_CLASS_ENTRY(ce, "Psr\\Link", "EvolvableLinkInterface", evolvableLinkInterface_class_fuctions);
+    evolvableLinkInterface = zend_register_internal_interface(&ce TSRMLS_CC);
+    zend_class_implements(evolvableLinkInterface, 1, linkInterface);
 
     INIT_NS_CLASS_ENTRY(ce, "Psr\\Link", "LinkCollectionInterface", linkCollectionInterface_class_fuctions);
     zend_register_internal_interface(&ce TSRMLS_CC);
